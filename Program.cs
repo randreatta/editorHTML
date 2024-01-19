2f5f85d5e341449671a211eb2b55e75f40f432f8
Rodrigo Andreatta <rodrigoandreattavieira@gmail.com>
2024-01-19 16:36:56 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace EditorHTML
+{
+    class Program 
+    {
+        static void Main(string[] args) {
+            Menu.Show();
+
+        }
+    }
+}
